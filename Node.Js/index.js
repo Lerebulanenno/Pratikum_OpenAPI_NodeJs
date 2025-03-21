14e62362229c07d0e22dd78dd870ad9d59d07f77
Enno Lerebulan <lerebulanenno@gmail.com>
2025-03-21 11:32:57 +0800

    Menambahkan pembacaan file OpenAPI YAML

--- Node.Js/index.js
+++ Node.Js/index.js
@@ -3,3 +3,6 @@ import mysql from 'mysql2'; //mengimpor swagger untuk dokumentasi API
 import swaggerUi from 'swagger-ui-express'; //mengimpor swagger untuk dokumentasi API
 import fs from 'fs'; //Mengimpor modul fs untuk membaca file
 import YAML from 'yaml'; //mengimpor YAML untuk membaca Spesifikasi OPENAPI
+
+//Membaca dan mengubah file OpenAPI (spec.yaml) menjadi object Javascript
+const swaggerDocument = YAML.parse(fs.readFileSync('./openapi/spec.yaml', utf8));
