503db2492bf44b50bcf6c7624eadc27c608ed92a
Enno Lerebulan <lerebulanenno@gmail.com>
2025-03-21 11:28:41 +0800

    Menambahkan comment untuk mengetahui fungsi library

--- Node.Js/index.js
+++ Node.Js/index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import mysql from 'mysql2';
-import swaggerUi from 'swagger-ui-express';
-import fs from 'fs';
-import YAML from 'yaml';
+import express from 'express'; //mengimpor Express untuk membuat server
+import mysql from 'mysql2'; //mengimpor swagger untuk dokumentasi API
+import swaggerUi from 'swagger-ui-express'; //mengimpor swagger untuk dokumentasi API
+import fs from 'fs'; //Mengimpor modul fs untuk membaca file
+import YAML from 'yaml'; //mengimpor YAML untuk membaca Spesifikasi OPENAPI
